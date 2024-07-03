f8fe08b2ecbcfcb8923d46933a5748c3fc1b72af
JohannaAlamov <johanna@imaginagroup.com>
2024-07-03 16:43:20 +0200

    Agregar archivos de proyecto.

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ApiFinShark.Models;
+
+namespace ApiFinShark.Data
+{
+    public class ApplicationDBContext : DbContext
+    { 
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        { 
+
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
